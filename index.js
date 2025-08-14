a7d9450d8f5b6191a584784162334b81d92dd5b7
Clara <cfanget@ensc.fr>
2025-08-14 11:02:06 -0400

    [UPDATE] - Rediraction galerie

--- index.js
+++ index.js
@@ -118,8 +118,8 @@ function draw() {
 
   if (animationFinished) {
     background(0);
-    //window.location.href = "pages/gallery.html";
-    //return;
+    window.location.href = "pages/gallery.html";
+    return;
   }
 }
 
