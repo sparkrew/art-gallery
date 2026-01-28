40e5c1e4f44d602162b7b36f3f1d6956d187ecc0
alireza nasirian <78487048+alireza-nasirian@users.noreply.github.com>
2026-01-28 13:38:02 -0500

    [feature] add sample rust art work

---
+++ art/rust/template/pkg/rust_art_template_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const render: (a: number, b: number, c: number) => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
